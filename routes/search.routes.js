66990a94b1169b7fc582e57c17f89dc8fb291956
Jose Roberto <juniorferreirarob@gmail.com>
2020-09-01 14:08:18 -0300

    update controller/contracts

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -1,2 +1,58 @@
 const { Router } = require("express");
-const router = new Router();
+const router = express.Router();
+
+const client = require("../config/config");
+
+//ADD NEW CONTRACTS
+router.post("/contratos", (req, res) => {
+  const { decription, status } = req.body;
+  const query = `
+    INSERT INTO contracts (decription, status) VALUES ( ${decription}, ${status})
+    `;
+  client.query(query, (err, res) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    res.status(200).json(res);
+    client.end();
+  });
+});
+
+//LISTAR NOVO CONTRATO
+router.get("/contratos", (req, res) => {
+  const { decription, status } = req.body;
+  const query = `
+      SELECT * FROM contracts   
+    `;
+  // UPDATE contracts SET ${description}, ${status} WHERE _id = 1
+  client.query(query, (err, res) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    res.status(200).json(res);
+    client.end();
+  });
+});
+
+//UPDATE CONTRATO
+router.post("/contratos/{_id}", (req, res) => {
+  const { decription, status } = req.body;
+  const { _id } = req.params;
+  const query = `
+      UPDATE contracts SET ${decription}, ${status} WHERE _id = ${_id}
+    `;
+
+  client.query(query, (err, res) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    res.status(200).json(res);
+    client.end();
+  });
+});
---
+++ service/ContractService.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// ADD UM NOVO CONTRATO
+exports.addContract = function (body) {
+  return new Promise(function (resolve, reject) {
+    resolve();
+  });
+};
+
+exports.findContractsByStatus = function (status) {
+  return new Promise(function (resolve, reject) {
+    var examples = {};
+    examples["application/json"] = [{}];
+  });
+};
+
+//UPDATE AN EXISTING CONTRACT
+exports.updateContract = function (body) {
+  return new Promise(function (resolve, reject) {
+    resolve();
+  });
+};
